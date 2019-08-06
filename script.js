8b4d914f31e5757cde55a04bbbfaf2fc11b2e100
mlbaraldi <markbass1@hotmail.com>
2019-08-06 16:54:23 -0300

    commit com criacao de botoes e ferramentas

--- script.js
+++ script.js
@@ -2,7 +2,10 @@
 let gridSize = 20;
 const container = document.querySelector(".container");
 const containerSize = parseInt(container.style.width, 10)
-let selectedColor = "black";
+var hue = 0
+var saturation = 0
+var brightness = 0
+var selectedColor = `hsl( ${hue}, ${saturation}%, ${brightness}%)`;
 
 //create grid function
 function setGrid() {
@@ -15,16 +18,32 @@ function setGrid() {
         cell.style.backgroundColor = "white";
         container.appendChild(cell);
         cell.addEventListener("mouseover",
-            changeColor)
+            actualColor)
         console.log("cell created");
     }
 }
 
 // colorBrush
-function changeColor() {
+function actualColor() {
     this.style.setProperty("background", selectedColor)
 }
 
+//changecolor
+function changeColor() {
+    selectedColor = window.prompt("write a color!")
+}
+
+//random pencil
+function randomColor() {
+    selectedColor = "#" + ((1 << 24) * Math.random() | 0).toString(16)
+}
+//transparent pencil
+function transparentColor() {
+    if (brightness < 100) {
+        brightness = brightness + 10;
+    }
+
+}
 //reset grid button
 function resetGrid() {
     while (container.firstChild) {
